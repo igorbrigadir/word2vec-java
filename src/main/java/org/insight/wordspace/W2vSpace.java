bd703af508b46d492998570de9520b7e9de45381
igorbrigadir <igor.brigadir@gmail.com>
2015-02-02 18:46:02 +0000

    TODO: Move loading code

--- src/main/java/org/insight/wordspace/W2vSpace.java
+++ src/main/java/org/insight/wordspace/W2vSpace.java
@@ -1,114 +1,110 @@
 package org.insight.wordspace;
 
-import java.io.File;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.PriorityQueue;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.insight.wordspace.util.StopWords;
+import org.insight.wordspace.util.Filters;
 import org.insight.wordspace.util.VectorMath;
-import org.insight.wordspace.util.W2VModelLoader;
-import org.insight.wordspace.util.WordSim;
 
 /*
  * A Java wrapper for W2v - Only Reads a pre trained model!
  */
-// public class W2vSpace extends HashMap<String, float[]> {
-
 public class W2vSpace extends WordVectorSpace<float[]> {
 
-  public boolean saveAsText(File output) {
-    for (Entry<String, float[]> entry : store.entrySet()) {
-      String word = String.format("%s %s\n", entry.getKey(), StringUtils.join(entry.getValue(), ' '));
-      try {
-        FileUtils.writeStringToFile(output, word, true);
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-        return false;
+  public static W2vSpace load(String word2vecModel) {
+    W2vSpace model = new W2vSpace();
+    try (DataInputStream ds = new DataInputStream(new BufferedInputStream(new FileInputStream(word2vecModel), 131072))) {
+      // Read header:
+      int numWords = Integer.parseInt(readString(ds));
+      int vecSize = Integer.parseInt(readString(ds));
+      // System.out.println(numWords + " Words, Vector size " + vecSize);
+      for (int i = 0; i < numWords; i++) {
+        // Word:
+        String word = readString(ds);
+        // Vector:
+        float[] vector = readFloatVector(ds, vecSize);
+        // Unit Vector
+        model.store.put(word, VectorMath.normalize(vector));
+      }
+    } catch (IOException e) {
+      System.err.println("ERROR: Failed to load model: " + word2vecModel);
+      e.printStackTrace();
+    }
+    return model;
+  }
+
+  /*
+   * Read a string from the binary model (System default should be UTF-8):
+   */
+  public static String readString(DataInputStream ds) throws IOException {
+    ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+    while (true) {
+      byte byteValue = ds.readByte();
+      if ((byteValue != 32) && (byteValue != 10)) {
+        byteBuffer.write(byteValue);
+      } else if (byteBuffer.size() > 0) {
+        break;
       }
     }
-    return true;
+    String word = byteBuffer.toString();
+    byteBuffer.close();
+    return word;
   }
 
-  public static W2vSpace normalizeModel(W2vSpace raw) {
-    W2vSpace norm = new W2vSpace();
-    for (Entry<String, float[]> e : raw.store.entrySet()) {
-      norm.store.put(e.getKey(), VectorMath.normalize(e.getValue()));
+  /*
+   * Read a Vector - Array of Floats from the binary model:
+   */
+  public static float[] readFloatVector(DataInputStream ds, int vectorSize) throws IOException {
+    // Vector is an Array of Floats...
+    float[] vector = new float[vectorSize];
+    // Floats stored as 4 bytes
+    byte[] vectorBuffer = new byte[4 * vectorSize];
+    // Read the full vector in a single chunk:
+    ds.read(vectorBuffer);
+    // Parse bytes into floats
+    for (int i = 0; i < vectorSize; i++) {
+      // & with 0xFF to get unsigned byte value as int
+      int byte1 = (vectorBuffer[(i * 4) + 0] & 0xFF) << 0;
+      int byte2 = (vectorBuffer[(i * 4) + 1] & 0xFF) << 8;
+      int byte3 = (vectorBuffer[(i * 4) + 2] & 0xFF) << 16;
+      int byte4 = (vectorBuffer[(i * 4) + 3] & 0xFF) << 24;
+      // Encode the 4 byte values (0-255) above into a single int
+      // Reverse bytes for endian compatibility
+      int reverseBytes = (byte1 | byte2 | byte3 | byte4);
+      vector[i] = Float.intBitsToFloat(reverseBytes);
     }
-    return norm;
+    return vector;
   }
 
-
-
-  // Adding several words:
-
-  public float[] sentenceVector(boolean filter, String sentence) {
-    return sentenceVector(filter, sentence.toLowerCase().split(" "));
-  }
-
-  public float[] sentenceVector(boolean filter, String... words) {
-    List<float[]> vectors = new ArrayList<float[]>();
-    for (String w : words) {
-
-      // Stopword filter:
-      if (filter && StopWords.isStopWord(w)) {
-        continue;
-      }
-
-      if (this.contains(w)) {
-        vectors.add(vector(w));
-      }
-    }
-    return VectorMath.addAll(vectors);
-  }
-
-
-
-  // Linear search k Nearest Neighbour:
-  public List<String> knnWords(String word, int k) {
-    if (!this.contains(word)) {
-      System.err.println("Out of vocab word: " + word);
-      return new ArrayList<String>();
-    }
-
-    List<WordSim> words = knn(word, k);
-    List<String> ret = new ArrayList<String>();
-    for (WordSim w : words) {
-      ret.add(w.getString());
-    }
-    return ret;
-  }
-
-
-  public List<WordSim> knn(String word, int k) {
-    return knn(word, vector(word), k);
-  }
-
-  // Internal! but can be called to get similarities!
-  public List<WordSim> knn(String word, float[] vec, int k) {
-    PriorityQueue<WordSim> kSimilarWords = new PriorityQueue<WordSim>(k * 2);
-
-    for (Entry<String, float[]> e : store.entrySet()) {
-      WordSim sim = new WordSim(e.getKey(), VectorMath.cosineSimilarity(vec, e.getValue()));
-      kSimilarWords.add(sim);
-    }
-
-    List<WordSim> col = new ArrayList<WordSim>();
-    // col.clear();
-    for (int i = 0; i < k; i++) {
-      WordSim ws = kSimilarWords.poll();
-      if (!ws.getString().equalsIgnoreCase(word)) {
-        col.add(ws);
-      }
-    }
-    return col;
-  }
-
+  /*
+   * // Linear search k Nearest Neighbour: public List<String> knnWords(String
+   * word, int k) { if (!this.contains(word)) {
+   * System.err.println("Out of vocab word: " + word); return new
+   * ArrayList<String>(); }
+   * 
+   * List<WordSim> words = knn(word, k); List<String> ret = new
+   * ArrayList<String>(); for (WordSim w : words) { ret.add(w.getString()); }
+   * return ret; }
+   * 
+   * 
+   * public List<WordSim> knn(String word, int k) { return knn(word,
+   * vector(word), k); }
+   * 
+   * // Internal! but can be called to get similarities! public List<WordSim>
+   * knn(String word, float[] vec, int k) { PriorityQueue<WordSim> kSimilarWords
+   * = new PriorityQueue<WordSim>(k * 2);
+   * 
+   * for (Entry<String, float[]> e : store.entrySet()) { WordSim sim = new
+   * WordSim(e.getKey(), VectorMath.cosineSimilarity(vec, e.getValue()));
+   * kSimilarWords.add(sim); }
+   * 
+   * List<WordSim> col = new ArrayList<WordSim>(); // col.clear(); for (int i =
+   * 0; i < k; i++) { WordSim ws = kSimilarWords.poll(); if
+   * (!ws.getString().equalsIgnoreCase(word)) { col.add(ws); } } return col; }
+   */
 
   // WITH PREFIX:
   /*
@@ -120,90 +116,106 @@ public class W2vSpace extends WordVectorSpace<float[]> {
    * true); } else { return knn(this.vector(word), k, prefix); } }
    */
 
-
-  public List<WordSim> knn(float[] vec, int k, boolean withScores, String prefix) {
-    PriorityQueue<WordSim> kSimilarWords = new PriorityQueue<WordSim>(k);
-    for (Entry<String, float[]> e : store.entrySet()) {
-
-      if (!e.getKey().startsWith(prefix)) {
-        continue;
-      }
-
-      WordSim sim = new WordSim(e.getKey(), VectorMath.cosineSimilarity(vec, e.getValue()));
-
-      kSimilarWords.add(sim);
-
-    }
-    List<WordSim> col = new ArrayList<WordSim>();
-    // col.clear();
-
-    for (int i = 0; i < (k + 1); i++) {
-      col.add(kSimilarWords.poll());
-    }
-    return col;
-  }
-
-
-
   /*
-   * WordsOnly
+   * public List<WordSim> knn(float[] vec, int k, boolean withScores, String
+   * prefix) { PriorityQueue<WordSim> kSimilarWords = new
+   * PriorityQueue<WordSim>(k); for (Entry<String, float[]> e :
+   * store.entrySet()) {
+   * 
+   * if (!e.getKey().startsWith(prefix)) { continue; }
+   * 
+   * WordSim sim = new WordSim(e.getKey(), VectorMath.cosineSimilarity(vec,
+   * e.getValue()));
+   * 
+   * kSimilarWords.add(sim);
+   * 
+   * } List<WordSim> col = new ArrayList<WordSim>(); // col.clear();
+   * 
+   * for (int i = 0; i < (k + 1); i++) { col.add(kSimilarWords.poll()); } return
+   * col; }
+   * 
+   * 
+   * 
+   * 
+   * public List<WordSim> knWords(float[] vec, int k) { PriorityQueue<WordSim>
+   * kSimilarWords = new PriorityQueue<WordSim>(k); for (Entry<String, float[]>
+   * e : store.entrySet()) {
+   * 
+   * if (e.getKey().startsWith("@") || e.getKey().startsWith("#")) { continue; }
+   * 
+   * WordSim sim = new WordSim(e.getKey(), VectorMath.cosineSimilarity(vec,
+   * e.getValue())); kSimilarWords.add(sim); } List<WordSim> col = new
+   * ArrayList<WordSim>(); for (int i = 0; i < k; i++) {
+   * col.add(kSimilarWords.poll()); } return col; }
+   * 
+   * void printSims(String w, List<WordSim> sims) { for (WordSim s : sims) {
+   * String str = String.format("%s %s %.4f", w, s.getString(), s.getDouble());
+   * System.out.println(str); } }
    */
 
-  public List<WordSim> knWords(float[] vec, int k) {
-    PriorityQueue<WordSim> kSimilarWords = new PriorityQueue<WordSim>(k);
-    for (Entry<String, float[]> e : store.entrySet()) {
-
-      if (e.getKey().startsWith("@") || e.getKey().startsWith("#")) {
-        continue;
-      }
-
-      WordSim sim = new WordSim(e.getKey(), VectorMath.cosineSimilarity(vec, e.getValue()));
-      kSimilarWords.add(sim);
-    }
-    List<WordSim> col = new ArrayList<WordSim>();
-    for (int i = 0; i < k; i++) {
-      col.add(kSimilarWords.poll());
-    }
-    return col;
-  }
-
-  void printSims(String w, List<WordSim> sims) {
-    for (WordSim s : sims) {
-      String str = String.format("%s %s %.4f", w, s.getString(), s.getDouble());
-      System.out.println(str);
-    }
-  }
-
   public static void main(String[] args) {
-    long startTime = System.nanoTime();
 
-    W2vSpace w2v = W2VModelLoader.load("/home/igor/git/word2vec-java/src/main/resources/vectors.bin");
+    W2vSpace w2v = new W2vSpace();
 
-    long estimatedTime = System.nanoTime() - startTime;
-    float secs = estimatedTime / 1000000000.0F;
-    System.out.println("TOTAL Load TIME: " + secs);
+    // System.out.println(w2v.sentenceVector("@quick #brown fox and",
+    // Filters.removeMentions));
 
+    // System.out.println(w2v.sentenceVector("@quick #brown fox and",
+    // Filters.removeHashtags));
 
-    startTime = System.nanoTime();
-
-    int i = 100;
-    for (String word : w2v.store.keySet()) {
-      w2v.knn(word, 20);
-      i--;
-      if (i < 0) {
-        break;
-      }
-    }
-
-    w2v.printSims("king", w2v.knn("king", 10));
-    w2v.printSims("queen", w2v.knn("queen", 10));
+    // System.out.println(w2v.sentenceVector("@quick #brown fox and",
+    // Filters.removeHashtags, Filters.removeMentions));
 
 
-    estimatedTime = System.nanoTime() - startTime;
-    secs = estimatedTime / 1000000000.0F;
-    System.out.println("TOTAL EXECUTION TIME: " + secs);
+    // System.out.println(w2v.sentenceVector("@quick #brown fox and",
+    // Filters.removeStopwords));
+
+    System.out.println(w2v.sentenceVector("@quick #brown fox and foo", Filters.removeHashtags, Filters.removeMentions, Filters.removeStopwords));
+
+    System.out.println(w2v.sentenceVector("@quick #brown fox and foo", Filters.removeWords, Filters.removeHashtags, Filters.removeMentions,
+        Filters.removeStopwords));
+
+    System.out.println("rm'F:"
+        + w2v.sentenceVector("@quick #brown fox and foo", Filters.removeWords.with("fox"), Filters.removeHashtags, Filters.removeMentions,
+            Filters.removeStopwords));
+
+    System.out.println("inv*:"
+        + w2v.sentenceVector("@quick #brown fox and foo", Filters.invertRemove, Filters.removeHashtags, Filters.removeMentions, Filters.removeStopwords));
+
+    System.out.println("inv&:"
+        + w2v.sentenceVector("@quick #brown #yellow fox and foo", Filters.invertRemove.with("and", "#brown"), Filters.removeHashtags, Filters.removeMentions,
+            Filters.removeStopwords));
+
+
+
+    System.out.println();
+
+    /*
+     * for (int j = 0; j < 10; j++) {
+     * 
+     * long startTime = System.nanoTime();
+     * 
+     * W2vSpace w2v = W2VModelLoader.load(
+     * "/home/igor/git/word2vec-java/src/main/resources/vectors.bin");
+     * 
+     * long estimatedTime = System.nanoTime() - startTime; float secs =
+     * estimatedTime / 1000000000.0F; System.out.println("TOTAL Load TIME: " +
+     * secs);
+     * 
+     * 
+     * startTime = System.nanoTime();
+     * 
+     * int i = 100; for (String word : w2v.store.keySet()) { w2v.knn(word, 20);
+     * i--; if (i < 0) { break; } }
+     * 
+     * w2v.printSims("king", w2v.knn("king", 10)); w2v.printSims("queen",
+     * w2v.knn("queen", 10));
+     * 
+     * 
+     * estimatedTime = System.nanoTime() - startTime; secs = estimatedTime /
+     * 1000000000.0F; System.out.println("TOTAL EXECUTION TIME: " + secs);
+     */
   }
-
   /*
    * 
    * W2vSpace w2v =
--- src/main/java/org/insight/wordspace/WordVectorSpace.java
+++ src/main/java/org/insight/wordspace/WordVectorSpace.java
@@ -1,12 +1,24 @@
 package org.insight.wordspace;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.insight.wordspace.util.Filters;
+import org.insight.wordspace.util.Filters.WordFilter;
 import org.insight.wordspace.util.VectorMath;
 
 public abstract class WordVectorSpace<T> {
 
+  /*
+   * Store it in memory:
+   */
   public Map<String, T> store = new HashMap<String, T>();
 
   public boolean contains(String word) {
@@ -20,19 +32,50 @@ public abstract class WordVectorSpace<T> {
 
   /*
    * Cosine Distance between 2 words:
+   * 
+   * public Double cosineDistance(String word1, String word2) { if
+   * (!contains(word1)) { // System.err.println( "Out of vocab word: " + word1
+   * ); // word1 = "</s>"; return Double.NaN; } if (!contains(word2)) { //
+   * System.err.println( "Out of vocab word: " + word2 ); // word2 = "</s>";
+   * return Double.NaN; } return VectorMath.cosineSimilarity(vector(word1),
+   * vector(word2)); }
    */
-  public Double cosineDistance(String word1, String word2) {
-    if (!this.contains(word1)) {
-      // System.err.println( "Out of vocab word: " + word1 );
-      // word1 = "</s>";
-      return Double.NaN;
+
+  public boolean saveAsText(File output) {
+    for (Entry<String, T> entry : store.entrySet()) {
+      String word = String.format("%s %s\n", entry.getKey(), StringUtils.join(entry.getValue(), ' '));
+      try {
+        FileUtils.writeStringToFile(output, word, true);
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+        return false;
+      }
     }
-    if (!this.contains(word2)) {
-      // System.err.println( "Out of vocab word: " + word2 );
-      // word2 = "</s>";
-      return Double.NaN;
+    return true;
+  }
+
+  public static W2vSpace normalizeModel(W2vSpace raw) {
+    W2vSpace norm = new W2vSpace();
+    for (Entry<String, float[]> e : raw.store.entrySet()) {
+      norm.store.put(e.getKey(), VectorMath.normalize(e.getValue()));
     }
-    return VectorMath.cosineSimilarity(vector(word1), vector(word2));
+    return norm;
+  }
+
+  // Adding several words:
+  public T sentenceVector(String sentence) {
+    return sentenceVector(sentence, Filters.removeStopwords);
+  }
+
+  public T sentenceVector(String sentence, WordFilter... filters) {
+    List<T> vectors = new ArrayList<T>();
+    for (String word : StringUtils.split(sentence, ' ')) {
+      if (Filters.apply(word, filters)) {
+        vectors.add(vector(word));
+      }
+    }
+    return VectorMath.addAll(vectors);;
   }
 
 }

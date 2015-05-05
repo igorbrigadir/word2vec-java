748c4be41ae71c9f393ae380f0773573c4b829da
igorbrigadir <igor.brigadir@gmail.com>
2015-05-05 15:04:30 +0000

    ri hack

--- src/main/java/org/insight/wordspace/RidxSpace.java
+++ src/main/java/org/insight/wordspace/RidxSpace.java
@@ -1,51 +1,111 @@
 package org.insight.wordspace;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
+import org.apache.commons.math3.ml.distance.EuclideanDistance;
 import org.insight.wordspace.util.Compression;
+import org.insight.wordspace.util.VectorMath;
+import org.jblas.DoubleMatrix;
+import org.jblas.FloatMatrix;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.UnsafeMemoryInput;
 
 /*
  * Random Indexing
  */
-public class RidxSpace extends GenericWordSpace<Integer[]> {
+public class RidxSpace extends GenericWordSpace<int[]> {
 
-  public static float[] convertVector(int[] input, boolean decompress) {
-    if (decompress) {
-      return convertVector(Compression.decompress(input, 5000));
-    } else {
-      return convertVector(input);
+  private static int vectorSize;
+
+  public RidxSpace(int vectorSize) {
+    RidxSpace.vectorSize = vectorSize;
+  }
+
+  @SuppressWarnings("unchecked")
+  public static RidxSpace load(String ridxSpaceModel, int vectorSize) {
+    RidxSpace model = new RidxSpace(vectorSize);
+    System.out.println("Loading from file: " + ridxSpaceModel);
+    try {
+      // KRYO Store:
+      HashMap<String, int[]> clazz = new HashMap<String, int[]>();
+      Kryo kryo = new Kryo();
+      kryo.register(clazz.getClass());
+      UnsafeMemoryInput input = new UnsafeMemoryInput(new FileInputStream(ridxSpaceModel));
+      clazz = kryo.readObject(input, clazz.getClass());
+      for (Entry<String, int[]> e : clazz.entrySet()) {
+        model.store.put(e.getKey(), Compression.decompress(e.getValue(), vectorSize));
+      }
+      input.close();
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
+    return model;
+  }
+
+
+  public static FloatMatrix convertVector(int[] input, boolean decompress) {
+    FloatMatrix f = new FloatMatrix(convertVector(Compression.decompress(input, vectorSize)));
+    return f;
   }
 
   public static float[] convertVector(int[] input) {
     if (input == null) {
-      return new float[5000]; // Or throw an exception?
+      return new float[vectorSize]; // Or throw an exception?
     }
-
     float[] output = new float[input.length];
-
     for (int i = 0; i < input.length; i++) {
       output[i] = input[i];
     }
     return output;
   }
 
-  @Override
-  public double cosineSimilarity(Integer[] vec1, Integer[] vec2) {
-    // TODO Auto-generated method stub
-    return 0;
-  }
 
   @Override
-  public double distanceSimilarity(Integer[] vec1, Integer[] vec2) {
-    // TODO Auto-generated method stub
-    return 0;
+  public double cosineSimilarity(int[] vec1, int[] vec2) {
+    DoubleMatrix d1 = new DoubleMatrix(copyFromIntArray(vec1));
+    DoubleMatrix d2 = new DoubleMatrix(copyFromIntArray(vec2));
+
+    d1 = VectorMath.normalize(d1);
+    d2 = VectorMath.normalize(d2);
+
+    return VectorMath.cosineSimilarity(d1, d2);
   }
 
+
   @Override
-  public Integer[] additiveSentenceVector(List<Integer[]> vectors) {
-    // TODO Auto-generated method stub
-    return null;
+  public double distanceSimilarity(int[] vec1, int[] vec2) {
+    EuclideanDistance e = new EuclideanDistance();
+    return e.compute(copyFromIntArray(vec1), copyFromIntArray(vec2));
   }
+
+
+  @Override
+  public int[] additiveSentenceVector(List<int[]> vectors) {
+
+    int[] ret = new int[vectorSize];
+
+    for (int[] vec : vectors) {
+      // todo
+    }
+
+    return ret;
+
+  }
+
+
+  public static double[] copyFromIntArray(int[] source) {
+    double[] dest = new double[source.length];
+    for (int i = 0; i < source.length; i++) {
+      dest[i] = source[i];
+    }
+    return dest;
+  }
+
 
 }
